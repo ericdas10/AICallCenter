d58d453cc69c668379f04e76374b41c8894d9eaa
ericdas10 <ericdas4@yahoo.com>
2025-11-14 09:31:54 +0200

    Initial commit

---
+++ src/main/java/dev/callcenterapp/CallCenterAppApplication.java
@@ -0,0 +1,13 @@
+package dev.callcenterapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallCenterAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CallCenterAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/callcenterapp/CallCenterAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.callcenterapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallCenterAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
